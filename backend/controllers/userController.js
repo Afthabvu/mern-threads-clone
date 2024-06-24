b666021f870716fad37b478faf365d593d29f08e
afthab <afthabvu@gmail.com>
2024-06-25 00:40:43 +0530

    completed updateUser and getUserProfle routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,23 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username })
+      .select("-password")
+      .select("-updatedAt");
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error from getUserProfile", error.message);
+  }
+};
+
 export const signupUser = async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
@@ -104,5 +121,33 @@ export const followUnfollowUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
     console.log("Error in followUnfollow User", error.message);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { name, email, username, password, profilePic, bio } = req.body;
+  const userId = req.user._id;
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(400).json({ message: "user not found" });
+    if (req.params.id !== userId.toString())
+      return res
+        .status(400)
+        .json({ message: "you cannot update other users profile" });
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+    await user.save();
+    res.status(200).json({ message: "Profile updated succesfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in update User", error.message);
   }
 };
