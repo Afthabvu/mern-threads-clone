6b1a42182ec316236b9691417f213701e17dc773
afthab <afthabvu@gmail.com>
2024-08-09 06:37:06 +0530

    likeUnlike functionality completed

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,10 +1,43 @@
 import React, { useState } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import useShowToast from "../hooks/useShowToast";
 
-const Actions = ({}) => {
-  const [liked, setLiked] = useState(false);
-  const handleLike = () => {
-    setLiked((prev) => !prev);
+const Actions = ({ post: post_ }) => {
+  const user = useRecoilValue(userAtom);
+  const showToast = useShowToast();
+  const [liked, setLiked] = useState(post_.likes.includes(user?._id));
+  const [post, setPost] = useState(post_);
+  const [isLiking, setIsLiking] = useState(false);
+  const handleLikeAndUnlike = async () => {
+    if (!user)
+      return showToast("Error", "You must be logged in to like a post");
+    if (isLiking) return;
+    setIsLiking(true);
+
+    try {
+      const res = await fetch(`/api/posts/like/${post._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      if (data.error) return showToast("Error", data.error, "error");
+      if (!liked) {
+        //add the id of current user to likes array
+        setPost({ ...post, likes: [...post.likes, user._id] });
+      } else {
+        setPost({ ...post, likes: post.likes.filter((id) => id !== user._id) });
+      }
+      setLiked(!liked);
+      console.log(data);
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsLiking(false);
+    }
   };
   return (
     <Flex flexDirection="column">
@@ -17,7 +50,7 @@ const Actions = ({}) => {
           role="img"
           viewBox="0 0 24 22"
           width="20"
-          onClick={handleLike}
+          onClick={handleLikeAndUnlike}
         >
           <path
             d="M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z"
@@ -46,6 +79,15 @@ const Actions = ({}) => {
         </svg>
         <RepostSVG />
         <ShareSVG />
+      </Flex>
+      <Flex gap={2} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          {post?.replies?.length} replies
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          {post?.likes?.length} likes
+        </Text>
       </Flex>
     </Flex>
   );
